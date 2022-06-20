6c083d0a243063dd28517461aa406f30db2fcff0
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-06-20 20:49:32 +0300

    updated db , and requires to fix a problem with .env config

--- web-app/backend/src/app.module.ts
+++ web-app/backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { UsersModule } from './users/users.module';
 import {User} from "./users/entities/user.entity";
 import { DeviceController } from './device/device.controller';
@@ -11,23 +12,40 @@ import { TypeModule } from './type/type.module';
 import { DeviceGroupModule } from './device-group/device-group.module';
 import { RoomModule } from './room/room.module';
 import { StatusModule } from './status/status.module';
-import {Device} from "./device/entities/device.entity";
-
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {DatabaseModule} from "./database.module";
 
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'database/test7',
+      type: 'mariadb',
+     // "type": process.env.DATABASE_DIALECT,
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'smartApp',
       autoLoadEntities: true,
      // entities: [Test],
       // we should not use it in the production
-      synchronize: true,
+      synchronize: false,
     }),UsersModule, DeviceModule, ManufacturerModule, TypeModule, DeviceGroupModule, RoomModule, StatusModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
+
+
+/*@Module({
+  imports: [
+  ConfigModule.forRoot({}),
+    DatabaseModule,UsersModule, DeviceModule, ManufacturerModule, TypeModule, DeviceGroupModule, RoomModule, StatusModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService,ConfigService],
+
+})*/
+
+
 export class AppModule {}
