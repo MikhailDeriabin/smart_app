9053a122b230ac63fa3238cfd611b04e4657da8e
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-06-24 21:54:32 +0300

    added grid container css system in bootsrap style

--- web-app/frontend/src/App.tsx
+++ web-app/frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-    hello
+
+       <Header></Header>
+
+
+
     </div>
   );
 }
---
+++ web-app/frontend/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="container">
+                <h1>SmartApp</h1>
+                <nav></nav>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
