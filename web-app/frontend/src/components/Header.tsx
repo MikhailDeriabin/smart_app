1b1faf9171d16fd3fc5ea267acd5a9df37aec56b
Leevi Koskinen <31744881+leba9999@users.noreply.github.com>
2022-06-28 12:34:00 +0300

    Merge remote-tracking branch 'origin/master'

--- web-app/frontend/src/components/Header.tsx
+++ web-app/frontend/src/components/Header.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import {useNavigate} from "react-router-dom";
 
-
-
 const Header = () => {
     const navigate = useNavigate();
     return (
@@ -16,6 +14,7 @@ const Header = () => {
         </div>
     );
 };
+
 export default Header;
 
 
--- web-app/frontend/src/pages/Main.tsx
+++ web-app/frontend/src/pages/Main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ThemeStore from "../store/ThemeStore";
 
-
 const Main = () => {
     return (
         <div>
