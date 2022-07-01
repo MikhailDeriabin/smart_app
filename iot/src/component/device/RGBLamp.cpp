41d62449e61852b9be5c5b90fc9216f0f9c9f6e0
Mikhail <mikko.deriabin@gmail.com>
2022-07-01 20:26:22 +0300

    add RGBLamp class with basic functionalities

--- iot/src/component/device/RGBLamp.cpp
+++ iot/src/component/device/RGBLamp.cpp
@@ -1,17 +1,50 @@
 #include "Arduino.h"
 #include "RGBLamp.h"
 #include "Device.h"
+#include "Lamp.h"
 #include "../Status.h"
 
-RGBLamp::RGBLamp(int pinNumber) : pinNumber(pinNumber){
+RGBLamp::RGBLamp(int redPinNumber, int greenPinNumber, int bluePinNumber) : redPinNumber(redPinNumber), greenPinNumber(greenPinNumber), bluePinNumber(bluePinNumber){
     Component::name = "RGBLamp";
+    this->redLamp = new Lamp(redPinNumber, true, 255);
+    this->greenLamp = new Lamp(greenPinNumber, true, 255);
+    this->blueLamp = new Lamp(bluePinNumber, true, 255);
 }
 
-void RGBLamp::turnOn(){     
+void RGBLamp::turnOn(){  
+    redLamp->turnOn();  
+    greenLamp->turnOn();
+    blueLamp->turnOn(); 
 }
 
 void RGBLamp::turnOff(){ 
+    redLamp->turnOff();  
+    greenLamp->turnOff();
+    blueLamp->turnOff();
 }
 
 void RGBLamp::pulse(float interval){
+    //Can not see blinking if the inteval is below 20 ms
+    if(interval < 20)
+        interval = 20;
+
+    turnOn();
+    delay(interval);
+    turnOff();
+    delay(interval);
+}
+
+void RGBLamp::setIntensivity(float intensivity, bool write){ 
+    setRedIntensivity(intensivity, write); 
+    setGreenIntensivity(intensivity, write);
+    setBlueIntensivity(intensivity, write);
+}
+void RGBLamp::setRedIntensivity(float intensivity, bool write=false){ redLamp->setIntensivity(intensivity, write); }
+void RGBLamp::setGreenIntensivity(float intensivity, bool write=false){ greenLamp->setIntensivity(intensivity, write); }
+void RGBLamp::setBlueIntensivity(float intensivity, bool write=false){ blueLamp->setIntensivity(intensivity, write); }
+
+void RGBLamp::setColor(float redIntensivity, float greenIntensivity, float blueIntensivity, bool write=false){
+    setRedIntensivity(redIntensivity, write);
+    setGreenIntensivity(greenIntensivity, write);
+    setBlueIntensivity(blueIntensivity, write);
 }
--- iot/src/component/device/RGBLamp.h
+++ iot/src/component/device/RGBLamp.h
@@ -2,6 +2,7 @@
 #define RGBLAMP_H
 
 #include "Device.h"
+#include "Lamp.h"
 
 /**
  * @brief The class represents RGB Lamp lamp.
@@ -10,21 +11,17 @@
  */
 class RGBLamp : public Device{
 private:
-    const int pinNumber;
+    const int redPinNumber, greenPinNumber, bluePinNumber;
+    Lamp* redLamp; 
+    Lamp* greenLamp; 
+    Lamp* blueLamp;
 public: 
     /**
      * @brief Construct a new RGBLamp object.
      * 
      * @param pinNumber pin number where lamp is attached
      */
-    RGBLamp(int pinNumber);
-
-    /**
-     * @brief The method make a pulse signal(turn on -> turn off) with the given interval.
-     * 
-     * @param interval pulse interval time in ms
-     */
-    void pulse(float interval);
+    RGBLamp(int redPinNumber, int greenPinNumber, int bluePinNumber);
 
     /**
      * @brief The method turn the lamp on.
@@ -35,6 +32,20 @@ public:
      * @brief The method turn the lamp off.
      */
     void turnOff() override;
+
+    /**
+     * @brief The method make a pulse signal(turn on -> turn off) with the given interval.
+     * 
+     * @param interval pulse interval time in ms
+     */
+    void pulse(float interval);
+
+    void setIntensivity(float intensivity, bool write=false);
+    void setRedIntensivity(float intensivity, bool write=false);
+    void setGreenIntensivity(float intensivity, bool write=false);
+    void setBlueIntensivity(float intensivity, bool write=false);
+    
+    void setColor(float redIntensivity, float greenIntensivity, float blueIntensivity, bool write=false);    
 };
 
 #endif
