60de4e6065144c63a438fc0b9256468be1eeebc4
Leevi Koskinen <leevi.koskinen2@metropolia.fi>
2022-07-14 13:15:52 +0300

    Menu icons
    Signed-off-by: Leevi Koskinen <leevi.koskinen2@metropolia.fi>

--- web-app/frontend/src/components/NavBar.tsx
+++ web-app/frontend/src/components/NavBar.tsx
@@ -1,6 +1,11 @@
 import {useState} from 'react';
 import {useNavigate} from "react-router-dom";
 import { ReactComponent as Menu } from '../img/menu_FILL0_wght400_GRAD0_opsz48.svg'
+import Home  from '../img/home_FILL0_wght400_GRAD0_opsz48.svg'
+import Devices  from '../img/devices_other_FILL0_wght400_GRAD0_opsz48.svg'
+import Groups  from '../img/group_work_FILL0_wght400_GRAD0_opsz48.svg'
+import Settings  from '../img/settings_FILL0_wght400_GRAD0_opsz48.svg'
+import Info  from '../img/info_FILL0_wght400_GRAD0_opsz48.svg'
 import navClasses from './NavBar.module.css';
 import {useTranslation} from "react-i18next";
 
@@ -24,19 +29,24 @@ const NavBar = () => {
                 <div className={`${navClasses.menu} ` + (visible ? `${navClasses.visible}`: `${navClasses.invisible}`)}>
                     <ul className={navClasses.menuitems}>
                         <li onClick={()=>navigate("/")}>
+                            <img alt={"House"} src={Home}/>
                             {t('home')}
                         </li>
                         <li onClick={()=>navigate("/devices")}>
+                            <img alt={"Couple devices"} src={Devices}/>
                             {t('devices')}
                         </li>
                         <li onClick={()=>navigate("/groups")}>
+                            <img alt={"Three dots inside circle"} src={Groups}/>
                             {t('groups')}
                         </li>
                         <div className={navClasses.separator}/>
                         <li onClick={()=>navigate("/settings")}>
+                            <img alt={"Gear"} src={Settings}/>
                             {t('settings')}
                         </li>
                         <li onClick={()=>navigate("/about")}>
+                            <img alt={"Exclamation mark inside circle"} src={Info}/>
                             {t('about')}
                         </li>
                     </ul>
