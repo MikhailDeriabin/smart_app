4a40fe4a4d8f1d42fdafd567dc5b52bd01b905f6
Leevi Koskinen <31744881+leba9999@users.noreply.github.com>
2022-07-14 12:56:10 +0300

    NavBar fixes
    Signed-off-by: Leevi Koskinen <leevi.koskinen2@metropolia.fi>

--- web-app/frontend/src/components/AppRouter.tsx
+++ web-app/frontend/src/components/AppRouter.tsx
@@ -4,6 +4,7 @@ import Error from "../pages/Error";
 import Main from "../pages/Main";
 import Devices from "../pages/Devices";
 import Groups from "../pages/Groups";
+import About from "../pages/About";
 
 const AppRouter = () => {
     return (
@@ -13,6 +14,7 @@ const AppRouter = () => {
                     <Route path="/settings" element={<Settings/>}/>
                     <Route path="/devices" element={<Devices/>}/>
                     <Route path="/groups" element={<Groups/>}/>
+                    <Route path="/about" element={<About/>}/>
                     <Route path="404"  element={<Error/>}/>
                     <Route
                         path="*"
