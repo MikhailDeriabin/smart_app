9cdf9c076654db1c0ad3875622e8f06cedd2ff82
Leevi Koskinen <31744881+leba9999@users.noreply.github.com>
2022-07-14 11:29:39 +0300

    Main pages added
    Main pages for application added

--- web-app/frontend/src/components/AppRouter.tsx
+++ web-app/frontend/src/components/AppRouter.tsx
@@ -2,6 +2,8 @@ import {Navigate, Route, Routes,} from "react-router-dom";
 import Settings from "../pages/Settings";
 import Error from "../pages/Error";
 import Main from "../pages/Main";
+import Devices from "../pages/Devices";
+import Groups from "../pages/Groups";
 
 const AppRouter = () => {
     return (
@@ -9,6 +11,8 @@ const AppRouter = () => {
                 <Routes>
                     <Route path="/" element={<Main/>}/>
                     <Route path="/settings" element={<Settings/>}/>
+                    <Route path="/devices" element={<Devices/>}/>
+                    <Route path="/groups" element={<Groups/>}/>
                     <Route path="404"  element={<Error/>}/>
                     <Route
                         path="*"
---
+++ web-app/frontend/src/pages/Groups.tsx
@@ -0,0 +1,11 @@
+
+
+const Groups = () => {
+    return (
+        <div>
+            <h1>Groups Page</h1>
+        </div>
+    );
+};
+
+export default Groups;
