23a22a0d15074e3a4f690ed33f0acf8867bf657c
Leevi Koskinen <31744881+leba9999@users.noreply.github.com>
2022-07-16 15:05:37 +0300

    Theme changes
    New test colors and variables. Icons changes color
    
    Signed-off-by: Leevi Koskinen <31744881+leba9999@users.noreply.github.com>

--- web-app/frontend/src/components/NavBar.tsx
+++ web-app/frontend/src/components/NavBar.tsx
@@ -1,16 +1,16 @@
 import {useState} from 'react';
 import {useNavigate} from "react-router-dom";
-import { ReactComponent as Menu } from '../img/menu_FILL0_wght400_GRAD0_opsz48.svg'
-import Home  from '../img/home_FILL0_wght400_GRAD0_opsz48.svg'
-import Devices  from '../img/devices_other_FILL0_wght400_GRAD0_opsz48.svg'
-import Groups  from '../img/group_work_FILL0_wght400_GRAD0_opsz48.svg'
-import Settings  from '../img/settings_FILL0_wght400_GRAD0_opsz48.svg'
-import Info  from '../img/info_FILL0_wght400_GRAD0_opsz48.svg'
+import {ReactComponent as Menu} from '../img/menu_FILL0_wght400_GRAD0_opsz48.svg'
+import {ReactComponent as Home} from '../img/home_FILL0_wght400_GRAD0_opsz48.svg'
+import {ReactComponent as Devices} from '../img/devices_other_FILL0_wght400_GRAD0_opsz48.svg'
+import {ReactComponent as Groups} from '../img/group_work_FILL0_wght400_GRAD0_opsz48.svg'
+import {ReactComponent as Settings} from '../img/settings_FILL0_wght400_GRAD0_opsz48.svg'
+import {ReactComponent as Info} from '../img/info_FILL0_wght400_GRAD0_opsz48.svg'
 import navClasses from './NavBar.module.css';
 import {useTranslation} from "react-i18next";
 
 const NavBar = () => {
-    const { t } = useTranslation();
+    const {t} = useTranslation();
     const navigate = useNavigate();
     const [visible, setVisible] = useState(false);
     console.log(visible)
@@ -18,7 +18,7 @@ const NavBar = () => {
         <nav className={navClasses.navbar}>
             <div className={navClasses.navbar_container}>
                 <button onClick={()=>setVisible(!visible)} className={navClasses.hamburger_lines}>
-                    <Menu className={navClasses.menuburger}/>
+                    <Menu fill={`var(--font-secondary-color)`} className={navClasses.menuburger}/>
                 </button>
                 <h1 className={navClasses.title}>Smart App</h1>
             </div>
@@ -29,24 +29,29 @@ const NavBar = () => {
                 <div className={`${navClasses.menu} ` + (visible ? `${navClasses.visible}`: `${navClasses.invisible}`)}>
                     <ul className={navClasses.menuitems}>
                         <li onClick={()=>navigate("/")}>
-                            <img alt={"House"} src={Home}/>
+                            <Home viewBox="0 0 48 48" fill={`var(--font-secondary-color)`}
+                                  className={navClasses.icons}/>
                             {t('home')}
                         </li>
                         <li onClick={()=>navigate("/devices")}>
-                            <img alt={"Couple devices"} src={Devices}/>
+                            <Devices viewBox="0 0 48 48" fill={`var(--font-secondary-color)`}
+                                     className={navClasses.icons}/>
                             {t('devices')}
                         </li>
                         <li onClick={()=>navigate("/groups")}>
-                            <img alt={"Three dots inside circle"} src={Groups}/>
+                            <Groups viewBox="0 0 48 48" fill={`var(--font-secondary-color)`}
+                                    className={navClasses.icons}/>
                             {t('groups')}
                         </li>
                         <div className={navClasses.separator}/>
                         <li onClick={()=>navigate("/settings")}>
-                            <img alt={"Gear"} src={Settings}/>
+                            <Settings viewBox="0 0 48 48" fill={`var(--font-secondary-color)`}
+                                      className={navClasses.icons}/>
                             {t('settings')}
                         </li>
                         <li onClick={()=>navigate("/about")}>
-                            <img alt={"Exclamation mark inside circle"} src={Info}/>
+                            <Info viewBox="0 0 48 48" fill={`var(--font-secondary-color)`}
+                                  className={navClasses.icons}/>
                             {t('about')}
                         </li>
                     </ul>
